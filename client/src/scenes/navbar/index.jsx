bd49a05f4662be302cb54877d50db007001a99de
Jaya <jayasingh.pat2gmail.com>
2023-09-15 19:37:23 +0530

    Friends working

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Friends
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
